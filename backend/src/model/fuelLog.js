5d052695e3719c997d010aba7db1b9ffd1be9c12
Sanskar <sethsanskar856@gmail.com>
2026-07-12 09:32:35 +0530

    expense and fuel log schema

---
+++ backend/src/model/fuelLog.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const fuelLogSchema = new mongoose.Schema({
+  vehicleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Vehicle',
+    required: true
+  },
+  tripId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Trip'
+  },
+  liters: {
+    type: Number,
+    required: true
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('FuelLog', fuelLogSchema);
