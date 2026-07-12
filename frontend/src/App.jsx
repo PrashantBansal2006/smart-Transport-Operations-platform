f2d5c42272f45af63f2deb3e1aca48d4bbdedd3a
Mr-Magic1 <vishwakarmakailash123@gmail.com>
2026-07-12 15:44:39 +0530

    Dashboard page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import SettingsPage from './pages/SettingsPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import VehicleRegistry from './pages/VehicleRegistry';
+import DashBoard from './pages/DashBoard';
 import './index.css';
 
 function App() {
@@ -20,13 +21,11 @@ function App() {
         {/* Protected Dashboard Routes */}
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/drivers" replace />} />
+            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<DashBoard />} />
             <Route path="drivers" element={<DriversPage />} />
-<<<<<<< HEAD
             <Route path="maintenance" element={<MaintenancePage />} />
-=======
             <Route path="fleet" element={<VehicleRegistry />} />
->>>>>>> f2395907721980fbbf272ce0ae60d7e4b6d637dd
             <Route path="settings" element={<SettingsPage />} />
             <Route path="*" element={<div className="text-on-surface">Page not found</div>} />
           </Route>
